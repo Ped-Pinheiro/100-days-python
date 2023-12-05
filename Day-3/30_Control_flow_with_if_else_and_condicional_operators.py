80ff87e37e733f2f66fef040202010a89a7bc960
Pedro Pinheiro <henlion99@gmail.com>
2023-12-05 19:19:27 -0300

    If/Else and Condicional operators

---
+++ Day-3/30_Control_flow_with_if_else_and_condicional_operators.py
@@ -0,0 +1,32 @@
+# This class is about if, else statement and conditional operators. 
+
+# Example:
+# I got my first job at an amusement park, my job was about writing a code that replaced a 
+# ticket box, so if a person wanted to go on roller coaster that person needed a height than 
+# 120cm
+
+print("Welcome to the rollercoaster !!")
+height = int(input("What is your height in cm? "))
+
+if height >= 120:
+    print("You can ride the rollercoaster!")
+else:
+    print("Sorry, you have to grow taller before you can ride")
+    
+# The if statement is about somenthing is TRUE and else FALSE.
+
+# Comparison Operators:
+
+# As we can see that code above, if we put a height to 120cm the output is on ELSE but if 
+# we put >= at the if statement, the result will be "You can ride the rollercoast".
+
+# Here are Operators and the Meaning:
+
+# Operator      Meaning
+#   >          Grater than
+#   <           Less than
+#   >=        Grater than or equal to
+#   <=        Less than or equal to
+#   ==          Equal to
+#   !=         Not equal to
+
